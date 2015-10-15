0cf0ea92263c1bc85898301c313af23803e19ef4
RoyTimmers <fhict@MacBook-Pro.local>
2015-10-15 14:04:33 +0200

    Shake

--- iOS8SwiftShakeGestureTutorial/iOS8SwiftShakeGestureTutorial/ViewController.swift
+++ iOS8SwiftShakeGestureTutorial/iOS8SwiftShakeGestureTutorial/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var shakeLabel: UILabel!
     
+    @IBOutlet weak var shakeLabel2: UILabel!
     override func canBecomeFirstResponder() -> Bool {
         return true
     }
@@ -19,19 +21,27 @@ class ViewController: UIViewController {
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
         if motion == .MotionShake{
             self.shakeLabel.text="Shaken, not stirred"
+            self.shakeLabel2.text="Shaking"
+            var timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "resetLabel", userInfo: nil, repeats: false)
         }
+    }
+    
+    func resetLabel()
+    {
+        self.shakeLabel.text="Shake me"
+        self.shakeLabel2.text="Just shaken"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
