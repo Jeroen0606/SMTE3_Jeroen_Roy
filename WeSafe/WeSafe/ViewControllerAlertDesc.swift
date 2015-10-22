3f97db9952c6f986e1f340c6ff45371e0bea00d6
RoyTimmers <fhict@MacBook-Pro.local>
2015-10-22 13:34:10 +0200

    asdfasd

---
+++ WeSafe/WeSafe/ViewControllerAlertDesc.swift
@@ -0,0 +1,39 @@
+//
+//  ViewControllerAlertDesc.swift
+//  WeSafe
+//
+//  Created by Fhict on 22/10/15.
+//  Copyright © 2015 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAlertDesc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    @IBAction func backButton(sender: AnyObject)  {
+        self.dismissViewControllerAnimated(true, completion:{})
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
